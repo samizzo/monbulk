8440e495768d419333a6ac862885dafd00398bb7
aglenn <aglenn@bafdac2c-dbdc-46f6-923a-4e95a7fa9079>
2012-05-25 11:01:37 +0000

    Added the svae function and TextArea implementation
    git-svn-id: http://medimage.versi.edu.au/repos/daris-monbulk/trunk@392 bafdac2c-dbdc-46f6-923a-4e95a7fa9079

--- src/monbulk/MethodBuilder/client/PreviewWindow.java
+++ src/monbulk/MethodBuilder/client/PreviewWindow.java
@@ -1,13 +1,26 @@
 package monbulk.MethodBuilder.client;
 
+
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ScrollPanel;
+import com.google.gwt.user.client.ui.TextArea;
 
+import monbulk.shared.Services.MethodService;
+import monbulk.shared.Services.ServiceRegistry;
+import monbulk.shared.Services.MethodService.MethodUpdateHandler;
+import monbulk.shared.util.MonbulkEnums.ServiceNames;
 import monbulk.shared.widgets.Window.IWindow;
 import monbulk.shared.widgets.Window.OkCancelWindow;
 
-public class PreviewWindow extends OkCancelWindow implements IWindow {
+public class PreviewWindow extends OkCancelWindow implements IWindow, MethodUpdateHandler {
 
 	private StringBuilder formattedText;
 	
@@ -27,7 +40,7 @@ public class PreviewWindow extends OkCancelWindow implements IWindow {
 		_framingPanel = new ScrollPanel();
 		//this.setWidget(_framingPanel);
 		m_contentPanel.add(_framingPanel);
-		_PreviewPanel = new HTMLPanel("No text to render");
+		_PreviewPanel = new HTMLPanel("");
 		_framingPanel.add(_PreviewPanel);
 		
 		
@@ -37,11 +50,87 @@ public class PreviewWindow extends OkCancelWindow implements IWindow {
 		formattedText = tmpList;
 		selectedFormat = inputFormat;
 		_PreviewPanel.clear();
-		HTML output = new HTML();
-		output.setHTML(tmpList.toString());
-		_PreviewPanel.add(output);
+		TextArea tmpArea = new TextArea();
+		tmpArea.setHeight("600px");
+		tmpArea.setWidth("600px");
+		//HTML output = new HTML();
+		//output.setHTML(tmpList.toString());
+		tmpArea.setText(formattedText.toString());
+		_PreviewPanel.add(tmpArea);
 		
 		
+		
+	}
+	public void loadPreview(String tmpList,PreviewWindow.SupportedFormats inputFormat)
+	{
+		
+		selectedFormat = inputFormat;
+		_PreviewPanel.clear();
+		
+		Button tmpButton = new Button();
+		tmpButton.setText("SAVE");
+		final TextArea tmpArea = new TextArea();
+		tmpArea.setHeight("600px");
+		tmpArea.setWidth("600px");
+		//HTML output = new HTML();
+		//output.setHTML(tmpList.toString());
+		tmpList = tmpList.replace("<method>", "");
+		tmpList = tmpList.replace("</method>", "");
+		tmpArea.setText(tmpList);
+		final MethodUpdateHandler tmpHandle = this;
+		tmpButton.addClickHandler(new ClickHandler(){
+
+			@Override
+			public void onClick(ClickEvent event) {
+				try
+				{
+					MethodService tmpSvc = MethodService.get();
+					if(tmpSvc != null)
+					{
+						tmpSvc.createOrUpdate(tmpArea.getText(), tmpHandle);
+							
+					}
+					else
+					{
+						throw new ServiceRegistry.ServiceNotFoundException(ServiceNames.Methods);
+					}
+				}
+				catch (ServiceRegistry.ServiceNotFoundException e)
+				{
+					GWT.log("Couldn't find Method service");
+				}
+				
+			}
+			
+		});
+		
+		Label Guide = new Label();
+		Guide.setText("You can append the xml of the method below. Click save to upload the method.");
+		_PreviewPanel.add(Guide);
+		_PreviewPanel.add(tmpButton);
+		_PreviewPanel.add(tmpArea);
+		
+		
+		
+	}
+	@Override
+	public void onUpdateMethod(String response) {
+		AbsolutePanel _w_response = new AbsolutePanel();
+		Label _r_text = new Label();
+		if(response!=null)
+		{
+			_r_text.setText("Success: Your method has been created with ID: " + response);
+		}
+		else
+		{
+			if(response=="")
+			{
+				_r_text.setText("Success: Your method has been updated");
+			}
+		}
+		_w_response.add(_r_text);
+		this._PreviewPanel.clear();
+		this._PreviewPanel.add(_w_response);
 		
 	}
 
